2ae4d7669a212498d146dd9f18c70ce9d042cc26
Дмитрий Дорошкевич <doroschkewich11@gmail.com>
2024-12-08 14:45:42 +0500

    Added the ability to trade items over limits.

--- Coop Trading/mods-unpacked/RobocrafterLP-Trade/mod_main.gd
+++ Coop Trading/mods-unpacked/RobocrafterLP-Trade/mod_main.gd
@@ -1,8 +1,8 @@
 extends Node
 
-const RobocrafterLP_Trade= "RobocrafterLP-Trade"
-const RobocrafterLP_Trade_DIR := RobocrafterLP_Trade
-const RobocrafterLP_Trade_LOG_NAME := RobocrafterLP_Trade + ":Main"
+const RobocrafterLP_Trade_ID = "RobocrafterLP-Trade"
+const RobocrafterLP_Trade_DIR := RobocrafterLP_Trade_ID
+const RobocrafterLP_Trade_LOG_NAME := RobocrafterLP_Trade_ID + ":Main"
 
 var mod_dir_path := ""
 var extensions_dir_path := ""
@@ -15,15 +15,67 @@ func _init() -> void:
 	# Add translations
 	add_translations()
 
+func _ready():
+	ModLoaderLog.info("Ready!", RobocrafterLP_Trade_LOG_NAME)	
+	_config()
+
+func _config()-> void: # Defaults for Mods Options
+	var data = ModLoaderStore.mod_data[RobocrafterLP_Trade_ID]
+
+	if data != null:
+		var version = data.manifest.version_number
+		ModLoaderLog.info("Current Version is %s." % version, RobocrafterLP_Trade_LOG_NAME)
+		var config = ModLoaderConfig.get_config(RobocrafterLP_Trade_ID, version)
+
+		if config == null:
+			var defaultConfig = ModLoaderConfig.get_default_config(RobocrafterLP_Trade_ID)
+			if defaultConfig != null:
+				config = ModLoaderConfig.create_config(RobocrafterLP_Trade_ID, version, defaultConfig.data)
+			else:
+				config = ModLoaderConfig.create_config(RobocrafterLP_Trade_ID, version, {})
+			
+		if config != null and ModLoaderConfig.get_current_config_name(RobocrafterLP_Trade_ID) != version:
+			ModLoaderConfig.set_current_config(config)
+			if config.is_valid():
+				config.save_to_file()
+				ModLoaderLog.info("Save config to : %s" % config.save_path, RobocrafterLP_Trade_LOG_NAME)
+	
+	var ModsConfigInterface = get_node("/root/ModLoader/dami-ModOptions/ModsConfigInterface")
+
+	if ModsConfigInterface != null:
+		ModLoaderLog.info("Connect setting_changed", RobocrafterLP_Trade_LOG_NAME)
+		ModsConfigInterface.connect("setting_changed", self, "setting_changed")
+	else:
+		ModLoaderLog.info("ModsConfigInterface is null", RobocrafterLP_Trade_LOG_NAME)
+	
+	
+func setting_changed(setting_name, value, _mod_nammod_namee)->void:
+	var config = ModLoaderConfig.get_current_config(RobocrafterLP_Trade_ID)
+
+	if config != null:
+		config.data[setting_name] = value;
+		config.save_to_file()
+
 func install_script_extensions() -> void:
 	extensions_dir_path = mod_dir_path.plus_file("extensions")
-	ModLoaderMod.install_script_extension(extensions_dir_path.plus_file("singletons/debug_service.gd"))
-	ModLoaderMod.install_script_extension(extensions_dir_path.plus_file("ui/menus/shop/coop_item_popup.gd"))
+
+	var extensions = [
+		"singletons/debug_service.gd",
+		"ui/menus/shop/coop_item_popup.gd"
+	]
+
+	# I don't think it makes much sense, but it looks cool
+	for extension in extensions:
+		ModLoaderMod.install_script_extension(extensions_dir_path.plus_file(extension))
 
 func add_translations() -> void:
 	translations_dir_path = mod_dir_path.plus_file("translations")
-	ModLoaderMod.add_translation(translations_dir_path.plus_file("RobocrafterLP-Trade.en.translation"))
-	ModLoaderMod.add_translation(translations_dir_path.plus_file("RobocrafterLP-Trade.ru.translation"))
 
-func _ready() -> void:
-	ModLoaderLog.info("Ready!", RobocrafterLP_Trade_LOG_NAME)
+	# Or maybe this is good practice?
+	var translations = [
+		"en",
+		"ru"
+	]
+
+	for translation in translations:
+		ModLoaderMod.add_translation(translations_dir_path.plus_file("RobocrafterLP-Trade.%s.translation" % translation))
